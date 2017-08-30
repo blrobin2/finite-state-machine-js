29f1b0d2d5fc10d1fca726dec991c32a7dfc38bc
Bennie <brobinson88@gmail.com>
2017-08-30 15:12:10 -0400

    Saving

---
+++ src/index.js
@@ -0,0 +1,79 @@
+import {h, render, Component} from 'preact'
+
+const machine = {
+  idle: {
+    FETCH: 'loading'
+  },
+  loading: {
+    RESOLVE: 'data',
+    REJECT: 'error'
+  },
+  data: {
+    FETCH: 'loading'
+  },
+  error: {
+    FETCH: 'loading'
+  }
+}
+
+const initialState = 'idle'
+const transition = state => action => machine => machine[state][action]
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      dataState: initialState,
+      data: undefined
+    }
+
+    this.transition = this.transition.bind(this)
+    this.fetchData = this.fetchData.bind(this)
+    this.commands = {
+      loading: this.fetchData
+    }
+  }
+
+  fetchData() {
+    fetch(`https://api.github.com/rate_limit`)
+      .then(data => data.json())
+      .then(data =>
+        this.setState({ data },
+          () => this.transition('RESOLVE')))
+      .catch(_ => this.transition('REJECT'))
+  }
+
+  transition(action) {
+    const { dataState } = this.state
+    const nextState = machine[dataState][action]
+    const command = this.commands[nextState]
+
+    this.setState({
+      dataState: nextState
+    }, command)
+  }
+
+  render(props, state) {
+    const { data, dataState } = state
+    const buttonText = {
+      idle: 'Fetch data',
+      loading: "Loading...",
+      error: 'Fetch fail, retry?',
+      data: 'Fetch more data'
+    }[dataState]
+
+    return (
+      <div>
+        {data && <span><strong>Rate Limit:</strong> {data.rate.limit}</span>}
+        {dataState === 'error' && <span>Error: data not found</span>}
+        <button
+          onClick={() => this.transition('FETCH')}
+          disabled={dataState === 'loading'}>
+          {buttonText}
+        </button>
+      </div>
+    )
+  }
+}
+
+render(<App />, document.getElementById('app'))
